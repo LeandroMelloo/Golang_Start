bc30c30182b64372b15d9d0a0f6b5d42879bd84f
Leandro Paulino de Mello <leandropaulino@brq.com>
2020-04-12 18:29:45 -0300

    implementação do for com golang

---
+++ go-workspace/src/for/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	x := 0
+	for x < 10 {
+		fmt.Println(x)
+		x++
+	}
+
+	for {
+		x++
+
+		if x == 50 {
+			continue
+		}
+
+		if x == 100 {
+			break
+		}
+	}
+}
